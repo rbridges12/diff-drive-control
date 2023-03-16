8d737f94fc6bd9d17337fd901b96893a7ee71fbd
alisonryckman <alison.ryckman@gmail.com>
2023-03-16 15:14:36 -0400

    sim working

--- simTrajectory.m
+++ simTrajectory.m
@@ -1,5 +1,3 @@
-% simTrajectory.m
-
 function [tout, yout] = simTrajectory(x)
     % TODO fill out
     R = 2; % Radius of each wheel
@@ -7,8 +5,8 @@ function [tout, yout] = simTrajectory(x)
     tf = 5;
 
     % have x = angular velocity spline? x = [vl1, ... vln, vr1, ... vrn, tf];
-    interp_vel_l = @(t) interp1(linspace(0, tf, numel(x/2)), x(1:end/2-1), t);
-    interp_vel_r = @(t) interp1(linspace(0, tf, numel(x/2)), x(end/2:end), t);
+    interp_vel_l = @(t) interp1(linspace(0, tf, numel(x)/2), x(1:end/2), t);
+    interp_vel_r = @(t) interp1(linspace(0, tf, numel(x)/2), x(end/2+1:end), t);
 
     % v = R(vr + vl)/2
     % w = R(vr - vl)/2
@@ -17,8 +15,8 @@ function [tout, yout] = simTrajectory(x)
     % TODO: We only use theta here?
     odefun = @(t, y) [R/2*(interp_vel_r(t) + interp_vel_l(t)) * cos(y(3)); 
         R/2*(interp_vel_r(t) + interp_vel_l(t)) * sin(y(3));
-        R/L * (interp_velr_(t) - interp_vel_l(t))];
-    [tout, yout] = ode45(odefun, [0, tf], [0;0;0]);
+        R/L * (interp_vel_r(t) - interp_vel_l(t))];
+    [tout, yout] = ode45(odefun, [0, tf], [0;0;pi/4]);
 
 
 end
